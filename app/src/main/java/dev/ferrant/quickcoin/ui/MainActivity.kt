bf6acea55d1dcabf26ac35280b84ff509b2661dd
Ivan Ferrant <ivan@i2b.studio>
2022-04-13 18:47:56 +0200

    chore: Replace Glide with Coil - Use Material 3

--- app/src/main/java/dev/ferrant/quickcoin/ui/MainActivity.kt
+++ app/src/main/java/dev/ferrant/quickcoin/ui/MainActivity.kt
@@ -3,10 +3,11 @@ package dev.ferrant.quickcoin.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material3.MaterialTheme
 import dagger.hilt.android.AndroidEntryPoint
-import dev.ferrant.quickcoin.R
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
+@OptIn(ExperimentalCoroutinesApi::class)
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
