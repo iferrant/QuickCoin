66ee144596c1e6dc463af0a1b83c98438f8686d8
Iván Ferrant <ivan.ferrant@williamhill.com>
2022-05-12 16:04:03 +0200

    feat: Create default theme wrapper

--- app/src/main/java/dev/ferrant/quickcoin/ui/QuickCoinApp.kt
+++ app/src/main/java/dev/ferrant/quickcoin/ui/QuickCoinApp.kt
@@ -1,10 +1,15 @@
 package dev.ferrant.quickcoin.ui
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import dev.ferrant.quickcoin.ui.search.*
+import dev.ferrant.quickcoin.ui.search.Search
+import dev.ferrant.quickcoin.ui.theme.QuickCoinTheme
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 @ExperimentalCoroutinesApi
@@ -22,4 +27,19 @@ fun QuickCoinApp(
         }
     }
 
+}
+
+@Composable
+fun QuickCoinApp(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit
+) {
+    QuickCoinTheme {
+        Surface(
+            color = MaterialTheme.colorScheme.background,
+            modifier = modifier.fillMaxSize(),
+        ) {
+            content()
+        }
+    }
 }
